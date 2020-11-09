fcc6c346fcd2cdb10164bd181b47f2bc2b848f1a
Artur <a.nurtdinow@yandex.ru>
2020-11-09 19:29:43 +0300

    Onboard

--- app/src/main/java/com/spbstu/reversemarket/MainActivity.kt
+++ app/src/main/java/com/spbstu/reversemarket/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.spbstu.reversemarket
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            toMainScreen()
+        }, 1000)
+    }
+
+    private fun toMainScreen() {
+        val intent = Intent(this, NavigationActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        startActivity(intent)
     }
 }
