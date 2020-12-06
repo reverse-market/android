527045981dba51df8eef605276b192690e89285d
rar130600 <rar130600@mail.ru>
2020-12-06 14:36:44 +0300

    fixed photos fixed addresses

--- app/src/main/java/com/spbstu/reversemarket/profile/settings/SettingsFragment.kt
+++ app/src/main/java/com/spbstu/reversemarket/profile/settings/SettingsFragment.kt
@@ -6,17 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.spbstu.reversemarket.R
-import kotlinx.android.synthetic.main.fragment_settigs.*
+import com.spbstu.reversemarket.buy.domain.Address
+import com.spbstu.reversemarket.buy.presentation.AddressAdapter
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment() {
+
+    private lateinit var addressList: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_settigs, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        addressList = view.findViewById(R.id.layout_address_list)
+        addressList.adapter = AddressAdapter(
+            provideAddresses(),
+            ::provideAddressClickListener
+        )
+
         return view
     }
 
@@ -25,5 +37,22 @@ class SettingsFragment : Fragment() {
         settings_back_button.setOnClickListener {
             findNavController().popBackStack()
         }
+    }
+
+    private fun provideAddresses(): List<Address> = listOf(
+        Address(
+            "Мой адрес",
+            "Ленинградская область, г. Санкт-Петербург, Невский просп. д.28, кв. 1, 190000",
+            "Иванов Иван Иванович"
+        ),
+        Address(
+            "Работа",
+            "Ленинградская область, г. Санкт-Петербург, Невский просп. д.35, кв. 30, 190000",
+            "Иванов Иван Иванович"
+        )
+    )
+
+    private fun provideAddressClickListener(position: Int) {
+
     }
 }
