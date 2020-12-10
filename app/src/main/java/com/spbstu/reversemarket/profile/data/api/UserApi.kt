61cc02e65833f68b6378aee8920c332a0bb6d074
Artur <a.nurtdinow@yandex.ru>
2020-12-10 13:41:38 +0300

    New api calls

--- app/src/main/java/com/spbstu/reversemarket/profile/data/api/UserApi.kt
+++ app/src/main/java/com/spbstu/reversemarket/profile/data/api/UserApi.kt
@@ -1,8 +1,6 @@
 package com.spbstu.reversemarket.profile.data.api
 
-import com.spbstu.reversemarket.profile.data.model.AddressBodyNoId
-import com.spbstu.reversemarket.profile.data.model.AddressBodyWithId
-import com.spbstu.reversemarket.profile.data.model.UserBody
+import com.spbstu.reversemarket.profile.data.model.*
 import io.reactivex.rxjava3.core.Observable
 import retrofit2.Response
 import retrofit2.http.*
@@ -26,4 +24,28 @@ interface UserApi {
         @Path("id") id: Int,
         @Body addressBodyNoId: AddressBodyNoId
     ): Observable<Response<Void>>
+
+    @GET("/user/favorites")
+    fun getFavorites(): Observable<Response<List<Favorite>>>
+
+    @POST("/user/favorites")
+    fun setFavorite(@Body id: Int): Observable<Response<Void>>
+
+    @GET("/user/favorites/{id}")
+    fun getFavorite(@Path("id") id: Int): Observable<Response<Favorite>>
+
+    @DELETE("/user/favorites/{id}")
+    fun deleteFavorite(@Path("id") id: Int): Observable<Response<Void>>
+
+    @GET("/user/orders_sell")
+    fun getOrdersSell(): Observable<Response<List<Order>>>
+
+    @GET("/user/orders_sell/{id}")
+    fun getOrderSell(@Path("id") id: Int): Observable<Response<Order>>
+
+    @GET("/user/orders_buy")
+    fun getOrdersBuy(): Observable<Response<List<Order>>>
+
+    @POST("/user/orders_buy")
+    fun addOrderBuy(@Path("id") id: Int): Observable<Response<Void>>
 }
