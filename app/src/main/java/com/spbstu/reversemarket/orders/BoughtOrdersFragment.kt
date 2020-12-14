f4b69e7fee70a4bd85e9eac6979e798845eaaef7
Artur <a.nurtdinow@yandex.ru>
2020-12-15 01:08:02 +0300

    Injection in orders

--- app/src/main/java/com/spbstu/reversemarket/orders/BoughtOrdersFragment.kt
+++ app/src/main/java/com/spbstu/reversemarket/orders/BoughtOrdersFragment.kt
@@ -5,11 +5,13 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.spbstu.reversemarket.R
+import com.spbstu.reversemarket.base.InjectionFragment
 import com.spbstu.reversemarket.orders.adapter.BoughtOrderAdapter
 import com.spbstu.reversemarket.orders.domain.BoughtOrder
+import com.spbstu.reversemarket.profile.presentation.ProfileViewModel
 import kotlinx.android.synthetic.main.fragment_bought_orders.*
 
-class BoughtOrdersFragment : Fragment(R.layout.fragment_bought_orders) {
+class BoughtOrdersFragment : InjectionFragment<ProfileViewModel>(R.layout.fragment_bought_orders) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/spbstu/reversemarket/orders/OrdersFragment.kt
+++ app/src/main/java/com/spbstu/reversemarket/orders/OrdersFragment.kt
@@ -2,12 +2,13 @@ package com.spbstu.reversemarket.orders
 
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.spbstu.reversemarket.R
+import com.spbstu.reversemarket.base.InjectionFragment
+import com.spbstu.reversemarket.profile.presentation.ProfileViewModel
 import kotlinx.android.synthetic.main.fragment_orders.*
 
-class OrdersFragment : Fragment(R.layout.fragment_orders) {
+class OrdersFragment : InjectionFragment<ProfileViewModel>(R.layout.fragment_orders) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
